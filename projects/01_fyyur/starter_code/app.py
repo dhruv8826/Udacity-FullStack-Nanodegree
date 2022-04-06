20cb95cb744514b8f1c6ca0be9b2d5ee101d61be
dhruv <me@dhruvsharma.com>
2022-04-06 16:37:59 +0530

    Implemented the TODO section for form create for Artist, Venue and Show.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -22,6 +23,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
+migrate = Migrate(app, db)
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -40,6 +42,14 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(300))
+    looking_for_talent = db.Column(db.Boolean)
+    description = db.Column(db.String)
+    genres = db.Column(db.String(120)) # this is a coma seperated string containing multiple values
+
+    def __repr__(self):
+        return f'<Venue ID: {self.id}, Name: {self.name}, City: {self.city}, State: {self.state}, Address: {self.address}, Phone: {self.phone}, Image Link: {self.image_link}, Facebook Link: {self.facebook_link}, Website Link: {self.website_link}, Looking For Telent: {self.looking_for_talent}, Description: {self.description}, Genres: [{self.genres.split}]>'
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -49,13 +59,33 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.String(120)) # this is a coma seperated string containing multiple values
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(300))
+    looking_for_venues = db.Column(db.Boolean)
+    description = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Artist ID: {self.id}, Name: {self.name}, City: {self.city}, State: {self.state}, Phone: {self.phone}, Image Link: {self.image_link}, Facebook Link: {self.facebook_link}, Website Link: {self.website_link}, Looking For Venue: {self.looking_for_venues}, Description: {self.description}, Genres: [{self.genres}]>'
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime())
+    
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    artist = db.relationship('Artist', backref='show')
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    venue = db.relationship('Venue', backref='show')
+
+    def __repr__(self):
+        return f'<Show ID: {self.id}, Start Time: {self.start_time}, Artist ID: {self.artist_id}, Venue ID: {self.venue_id}>'
 
 #----------------------------------------------------------------------------#
 # Filters.

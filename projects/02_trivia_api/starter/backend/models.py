75b0e06fe01f5b6ce460a9f57644bc54198b976f
dhruv <me@dhruvsharma.com>
2022-05-07 16:30:45 +0530

    miscellaneous changes

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -2,11 +2,12 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_name = "trivia"
+database_name = DB_NAME
 # database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
-user_name = '' # username goes here
-user_password = '' # password goes here
+user_name = DB_USER
+user_password = DB_PASSWORD
 database_path = "postgresql://{}:{}@{}/{}".format(user_name, user_password, 'localhost:5432', database_name)
 
 db = SQLAlchemy()
